136be214e9ddc2c75da0d43add9327f812f33d37
thelegend30 <luibichenko.oleg@gmail.com>
2025-06-15 18:30:18 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = ["Learn Nothing ⚛️", "Be homeless 💼", "Git money🤑"];
+
+export default function App() {
+  // return <Steps />;
+  return <Counter />;
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen((is) => !is);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950fa", color: "#fff" }}
+              onClick={() => handlePrevious()}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950fa", color: "#fff" }}
+              onClick={() => handleNext()}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  function handleStepSub() {
+    setStep((s) => s - 1);
+  }
+
+  function handleStepAdd() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountSub() {
+    setCount((c) => c - step);
+  }
+
+  function handleCountAdd() {
+    setCount((c) => c + step);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <button onClick={handleStepSub}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleStepAdd}>+</button>
+      </div>
+      <div>
+        <button onClick={handleCountSub}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleCountAdd}>+</button>
+      </div>
+      <div>
+        {} is {}
+      </div>
+    </div>
+  );
+}
